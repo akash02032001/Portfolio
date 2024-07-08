0817bb0a536ba56cb3e7be82eb80bfe63818e6ef
unknown <www.akashmurugan@gmail.com>
2024-07-08 11:21:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var tablinks = document.getElementsByClassName("tablinks");
+        var tabcontents = document.getElementsByClassName("tabcontents");
+        function opentab(tabname){
+            for(tablink of tablinks){
+                tablink.classList.remove("activelink");
+            }
+            for(tabcontent of tabcontents){
+                tabcontent.classList.remove("activetab");
+            }
+            event.currentTarget.classList.add("activelink")
+            document.getElementById(tabname).classList.add("activetab")
+        }
+
+        window.addEventListener("scroll",function(){
+            const header = document.querySelector("header");
+            header.classList.toggle("sticky",window.scrollY > 0);
+        });
+
